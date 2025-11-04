657dade62b166508fbebc86d2ac2c88e76c3d349
CarlosAvila31 <carrosavila@gmail.com>
2025-11-03 19:43:00 -0600

    Añadi los templates para la parte administrativa

--- salon_fiestas/app/urls.py
+++ salon_fiestas/app/urls.py
@@ -6,5 +6,7 @@ from . import views
 urlpatterns = [
 
     path('', views.inicio, name='inicio'),
-    
+    path('crear/', views.crear_reservacion, name='crear'),
+    path('editar/<int:id>/', views.editar_reservacion, name='editar'),
+    path('eliminar/<int:id>/', views.eliminar_reservicion, name='eliminar'),
 ]
--- salon_fiestas/app/views.py
+++ salon_fiestas/app/views.py
@@ -6,3 +6,35 @@ from .models import RESERVACION, SHOW
 
 def inicio(request):
     return render(request, 'index.html')
+
+def crear_reservacion(request):
+    if request.method == 'POST':
+        nombre_cliente = request.POST['nombre_cliente']
+        fecha_evento = request.POST['fecha_evento']
+        num_invitados = request.POST['num_invitados']
+        tipo_evento = request.POST['tipo_evento']
+        telefono_contacto = request.POST['telefono_contacto']
+        estatus = request.POST['estatus']
+
+        RESERVACION.objects.create(nombre_cliente=nombre_cliente, fecha_evento=fecha_evento, num_invitados=num_invitados, tipo_evento=tipo_evento, telefono_contacto=telefono_contacto, estatus=estatus)
+        return redirect('listar')
+    return render(request, 'crear.html')
+
+def editar_reservacion(request, id):
+    reservacion = get_object_or_404(RESERVACION, id=id)
+    if request.method == 'POST':
+        reservacion.nombre_cliente = request.POST['nombre_cliente']
+        reservacion.fecha_evento = request.POST['fecha_evento']
+        reservacion.num_invitados = request.POST['num_invitados']
+        reservacion.tipo_evento = request.POST['tipo_evento']
+        reservacion.telefono_contacto = request.POST['telefono_contacto']
+        reservacion.estatus = request.POST['estatus']
+
+        reservacion.save()
+        return redirect('listar')
+    return render(request, 'editar.html', {'reservacion': reservacion})
+
+def eliminar_reservicion(request, id):
+    reservacion = get_object_or_404(RESERVACION, id=id)
+    reservacion.delete()
+    return redirect('listar')

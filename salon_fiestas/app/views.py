578b67283dcb4b99430bbe4e0d3ff7cc6aad62b1
Brissa Marisol Flores Tamez <brissa.florest@uanl.edu.mx>
2025-11-07 18:15:55 -0600

    User Login Queveriento

--- salon_fiestas/app/views.py
+++ salon_fiestas/app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import RESERVACION, SHOW
+from django.contrib.auth.views import LoginView
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 
@@ -10,6 +13,10 @@ def inicio(request):
 def listar_reservaciones(request):
     reservaciones = RESERVACION.objects.all()
     return render(request, 'listar.html', {'reservaciones': reservaciones})
+
+def listar_empleado(request):
+    reservaciones = RESERVACION.objects.all()
+    return render(request, 'listar_empleado.html', {'reservaciones': reservaciones})
 
 def crear_reservacion(request):
     if request.method == 'POST':
@@ -42,3 +49,30 @@ def eliminar_reservicion(request, id):
     reservacion = get_object_or_404(RESERVACION, id=id)
     reservacion.delete()
     return redirect('listar')
+
+class RoleLoginForm(AuthenticationForm):
+    role = forms.ChoiceField(
+        choices=[('admin', 'Administrador'), ('empleado', 'Empleado')],
+        label='Tipo de usuario'
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['role'].widget.attrs.update({'id': 'id_role'})
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    authentication_form = RoleLoginForm
+
+    def form_valid(self, form):
+        role = self.request.POST.get('role')
+        if role == 'admin':
+            return redirect('listar')
+        else:
+            return redirect('listar_empleado')
+        
+def marcar_listo(request, id):
+    reservacion = get_object_or_404(RESERVACION, id=id)
+    reservacion.estatus = "listo"
+    reservacion.save()
+    return redirect('listar_empleado')

b8cf74528fc46cbd34fe79a6b658aa3b4892da43
CarlosAvila31 <carrosavila@gmail.com>
2025-11-07 20:49:25 -0600

    Añadí validaciones al crud

--- salon_fiestas/app/views.py
+++ salon_fiestas/app/views.py
@@ -3,6 +3,8 @@ from .models import RESERVACION, SHOW
 from django.contrib.auth.views import LoginView
 from django import forms
 from django.contrib.auth.forms import AuthenticationForm
+from datetime import datetime 
+
 
 # Create your views here.
 
@@ -23,9 +25,30 @@ def crear_reservacion(request):
         nombre_cliente = request.POST['txtNombre_Cliente']
         fecha_evento = request.POST['txtFecha']
         num_invitados = request.POST['txtNum_Inv']
-        tipo_evento = request.POST['txtTipo_Evento']
+        tipo_evento = request.POST['ddlTipo_Evento']
         telefono_contacto = request.POST['txtTelefono_Contacto']
         estatus = request.POST['ddlestatus']
+        
+        errores = {}
+        
+        fecha_evento_dt = datetime.strptime(fecha_evento, '%Y-%m-%dT%H:%M')
+        if fecha_evento_dt < datetime.now():
+            errores['txtFecha'] = 'No se puede registrar una fecha anterior a la actual.'
+            return render(request, 'crear.html', {'errores': errores})
+        
+        num_invitados = int(request.POST['txtNum_Inv'], 0)
+        if num_invitados <= 0:
+            errores['txtNum_Inv'] = 'El número de invitados debe ser un entero positivo.'
+            return render(request, 'crear.html', {'errores': errores})
+
+        if RESERVACION.objects.filter(telefono_contacto=telefono_contacto).exists():
+            errores['txtTelefono_Contacto'] = 'Este número ya está registrado.'
+            return render(request, 'crear.html', {'errores': errores})
+        if RESERVACION.objects.filter(fecha_evento=fecha_evento).exists():
+            errores['txtFecha'] = 'Esta fecha ya está registrada.'
+            return render(request, 'crear.html', {'errores': errores})
+        
+        
 
         RESERVACION.objects.create(nombre_cliente=nombre_cliente, fecha_evento=fecha_evento, num_invitados=num_invitados, tipo_evento=tipo_evento, telefono_contacto=telefono_contacto, estatus=estatus)
         return redirect('listar')
@@ -37,9 +60,28 @@ def editar_reservacion(request, id):
         reservacion.nombre_cliente = request.POST['txtNombre_Cliente']
         reservacion.fecha_evento = request.POST['txtFecha']
         reservacion.num_invitados = request.POST['txtNum_Inv']
-        reservacion.tipo_evento = request.POST['txtTipo_Evento']
+        reservacion.tipo_evento = request.POST['ddlTipo_Evento']
         reservacion.telefono_contacto = request.POST['txtTelefono_Contacto']
         reservacion.estatus = request.POST['ddlestatus']
+        
+        errores = {}
+        fecha_evento_dt = datetime.strptime(reservacion.fecha_evento, '%Y-%m-%dT%H:%M')
+        if fecha_evento_dt < datetime.now():
+            errores['txtFecha'] = 'No se puede registrar una fecha anterior a la actual.'
+            return render(request, 'editar.html', {'reservacion': reservacion, 'errores': errores})
+        
+        num_invitados = int(request.POST['txtNum_Inv'], 0)
+        if num_invitados <= 0:
+            errores['txtNum_Inv'] = 'El número de invitados debe ser un entero positivo.'
+            return render(request, 'editar.html', {'reservacion': reservacion, 'errores': errores})
+
+        if RESERVACION.objects.filter(telefono_contacto=request.POST['txtTelefono_Contacto']).exclude(id=reservacion.id).exists():
+            errores['txtTelefono_Contacto'] = 'Este número ya está registrado.'
+            return render(request, 'editar.html', {'reservacion': reservacion, 'errores': errores})
+        if RESERVACION.objects.filter(fecha_evento=request.POST['txtFecha']).exclude(id=reservacion.id).exists():
+            errores['txtFecha'] = 'Esta fecha ya está registrada.'
+            return render(request, 'editar.html', {'reservacion': reservacion, 'errores': errores})
+        
 
         reservacion.save()
         return redirect('listar')
